759c44ed5f5b5f38e6246c8d26129274b2bd5de7
Fernando Fuertes <fer@desprogramar.com>
2026-01-06 13:29:38 +0100

    Lo probamos y todo ok
    Probamos con "npm run dev"

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
+import { ProductCard } from "../lib/components";
+import "../lib/styles.css";
+
+import coffeeMug from "./coffee-mug.png";
+
+const product = {
+  id: "1",
+  title: "Coffe Mug - Card",
+  image: coffeeMug,
+};
+
 export const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="my-4 mx-16">
+      <h1 className="text-4xl font-bold pb-4 border-b mb-8">Npm Deploy</h1>
+
+      <ProductCard product={product} initialValues={{ count: 4, maxCount: 10 }}>
+        {() => (
+          <>
+            <ProductCard.Image />
+            <ProductCard.Title />
+            <ProductCard.Buttons />
+          </>
+        )}
+      </ProductCard>
+    </div>
+  );
 };
